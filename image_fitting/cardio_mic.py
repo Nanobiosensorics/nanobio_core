d76b23a10515e5dcf0b366dacc2a259f07f58500
Bálint Béres <balintberes96@gmail.com>
2023-08-14 17:43:36 +0200

    mic scale tuning based on point reference

--- image_fitting/cardio_mic.py
+++ image_fitting/cardio_mic.py
@@ -1,22 +1,30 @@
 import matplotlib.pyplot as plt
-from ..epic_cardio.math_ops import get_max_well
 import numpy as np
 import cv2
 import os
+from scipy.spatial.distance import euclidean
+
 
 class CardioMicScaling:
     MIC_5X = 2134 / 2.22
+    MIC_10X = 2134 * 0.9817
     MIC_20X = 2134 * 1.81
 
 class CardioMicFitter:
     def __init__(self, well, mic, result_path, scaling=CardioMicScaling.MIC_5X, block=True):
         self.closed = False
+        self.tuned = False
+        self.cardio_points = [False, np.zeros((2,2))]
+        self.mic_points = [False, np.zeros((2,2))]
         self._well_id = 0
         self.translation = np.array([1420, 955])
         self.distance = 100
         self.result_path = result_path
-        self.scale, _ = self._get_scale(scaling)
-        self._mic, self._well = mic, cv2.resize(get_max_well(well), (self.scale, self.scale), interpolation=cv2.INTER_NEAREST)
+        self.scale = self._get_scale(scaling)
+        img = well[-1].copy()
+        img[img < 0] = 0
+        self.max_well = img
+        self._mic, self._well = mic, cv2.resize(self.max_well, (self.scale, self.scale), interpolation=cv2.INTER_NEAREST)
         
         self._fig, self._ax = plt.subplots(figsize=(16, 8))
         self._ax.set_axis_off()
@@ -24,19 +32,21 @@ class CardioMicFitter:
         self._elm = self._ax.imshow(self._well, alpha=.6,
                     extent = [self.translation[0], self.translation[0]  + self._well.shape[0],
                                                self.translation[1] + self._well.shape[1], self.translation[1]],)
+        self.pts1, = self._ax.plot((0,0), (0,0), 'bo')
+        self.pts2, = self._ax.plot((0,0), (0,0), 'ro')
         self._fig.canvas.mpl_connect('key_press_event', self.on_press)
         self._fig.canvas.mpl_connect('button_press_event', self.on_press)
-        self._ax.set_title(f'Current translation {self.translation}, speed {self.distance}')
+        self.make_title()
         self.draw_plot()
         plt.show(block=block)
         
     def _get_scale(self, scaling):
         MIC_PX_PER_UM = scaling / 1000
         EPIC_PX_PER_UM = 1/25
-        EPIC_CARDIO_SCALE = int(80 * (MIC_PX_PER_UM / EPIC_PX_PER_UM) * 0.978)
+        EPIC_CARDIO_SCALE = int(80 * (MIC_PX_PER_UM / EPIC_PX_PER_UM))
         MIC_UM_PER_PX = 1 / MIC_PX_PER_UM
         MIC_PX_AREA = MIC_UM_PER_PX**2
-        return EPIC_CARDIO_SCALE, MIC_UM_PER_PX
+        return EPIC_CARDIO_SCALE
 
     def draw_plot(self):
         self._elm.set_extent([self.translation[0], self.translation[0]  + self._well.shape[0],
@@ -59,49 +69,74 @@ class CardioMicFitter:
         self.closed = True
         
     def handle_key_press(self, evt):
-        if hasattr(evt, 'button'):
-            if evt.xdata != None and evt.ydata != None:
-                self.translation[0] = round(evt.xdata)
-                self.translation[1] = round(evt.ydata)
-                self.draw_plot()
-        elif evt.key != None:
-            if evt.key == 'right':
-                self.translation[0] = self.translation[0] + self.distance # if translation[0] + distance < im_dst.shape[1] else im_dst.shape[1]
-                self.draw_plot()
-            elif evt.key == 'left':
-                self.translation[0] = self.translation[0] - self.distance # if translation[0] - distance > 0 else 0
-                self.draw_plot()
-            elif evt.key == 'down':
-                self.translation[1] = self.translation[1] + self.distance # if translation[1] + distance < im_dst.shape[0] else im_dst.shape[0]
-                self.draw_plot()
-            elif evt.key == 'up':
-                self.translation[1] = self.translation[1] - self.distance # if translation[1] - distance > 0 else 0
-                self.draw_plot()
-            elif evt.key == '1':
-                self.distance = 1
-            elif evt.key == '2':
-                self.distance = 5
-            elif evt.key == '3':
-                self.distance = 10
-            elif evt.key == '4':
-                self.distance = 20
-            elif evt.key == '5':
-                self.distance = 50
-            elif evt.key == '6':
-                self.distance = 100
-            elif evt.key == '7':
-                self.distance = 200
-            elif evt.key == '8':
-                self.distance = 500
-            elif evt.key == 'n':
-                self.next_pressed()        
-            elif evt.key == 'enter':
-                self.enter_pressed()
+        if self.tuned:
+            if evt.key == 'm' and hasattr(evt, 'button'):
+                if evt.xdata != None and evt.ydata != None:
+                    self.translation[0] = round(evt.xdata)
+                    self.translation[1] = round(evt.ydata)
+                    self.draw_plot()
+            elif evt.key != None:
+                if evt.key == 'right':
+                    self.translation[0] = self.translation[0] + self.distance # if translation[0] + distance < im_dst.shape[1] else im_dst.shape[1]
+                    self.draw_plot()
+                elif evt.key == 'left':
+                    self.translation[0] = self.translation[0] - self.distance # if translation[0] - distance > 0 else 0
+                    self.draw_plot()
+                elif evt.key == 'down':
+                    self.translation[1] = self.translation[1] + self.distance # if translation[1] + distance < im_dst.shape[0] else im_dst.shape[0]
+                    self.draw_plot()
+                elif evt.key == 'up':
+                    self.translation[1] = self.translation[1] - self.distance # if translation[1] - distance > 0 else 0
+                    self.draw_plot()
+                elif evt.key in '12345678':
+                    if evt.key == '1':
+                        self.distance = 1
+                    elif evt.key == '2':
+                        self.distance = 5
+                    elif evt.key == '3':
+                        self.distance = 10
+                    elif evt.key == '4':
+                        self.distance = 20
+                    elif evt.key == '5':
+                        self.distance = 50
+                    elif evt.key == '6':
+                        self.distance = 100
+                    elif evt.key == '7':
+                        self.distance = 200
+                    elif evt.key == '8':
+                        self.distance = 500
+                    self.make_title()
+                    self._fig.canvas.draw()
+                elif evt.key == 'n':
+                    self.next_pressed()        
+                elif evt.key == 'enter':
+                    self.enter_pressed()
+        else:
+            if hasattr(evt, 'button'):
+                if evt.xdata != None and evt.ydata != None:
+                    if evt.key == 'a':
+                        self.cardio_points[1][1 if self.cardio_points[0] else 0] = (evt.xdata, evt.ydata)
+                        self.cardio_points[0] = not self.cardio_points[0]
+                        self.pts1.set_xdata(self.cardio_points[1][:,0])
+                        self.pts1.set_ydata(self.cardio_points[1][:,1])
+                        self._fig.canvas.draw()
+                    elif evt.key == 'd':
+                        self.mic_points[1][1 if self.mic_points[0] else 0] = (evt.xdata, evt.ydata)
+                        self.mic_points[0] = not self.mic_points[0]
+                        self.pts2.set_xdata(self.mic_points[1][:,0])
+                        self.pts2.set_ydata(self.mic_points[1][:,1])
+                        self._fig.canvas.draw()
+            elif evt.key == 't':
+                self.scale *= euclidean(self.mic_points[1][0], self.mic_points[1][1]) / euclidean(self.cardio_points[1][0], self.cardio_points[1][1])
+                self.scale = int(self.scale)
+                self._well = cv2.resize(self.max_well, (self.scale, self.scale), interpolation=cv2.INTER_NEAREST)
+                self.tuned = True
+                self.pts1.remove()
+                self.pts2.remove()
+                self.draw_plot()                        
         
     def on_press(self, evt):
         self.handle_key_press(evt)
-        self.make_title()
-        self._fig.canvas.draw()
         
 class CardioMicFitterMultipleWell(CardioMicFitter):
     _ids = [ 'A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4']
@@ -118,8 +153,10 @@ class CardioMicFitterMultipleWell(CardioMicFitter):
         if self._well_id == len(self._ids):
             plt.close(self._fig)
             self.closed = True
-        else:
-            self._well = cv2.resize(get_max_well(self._wells_data[self._ids[self._well_id]]), (self.scale, self.scale), interpolation=cv2.INTER_NEAREST)
+        else:        
+            img = self._wells_data[self._ids[self._well_id]][-1].copy()
+            img[img < 0] = 0
+            self._well = cv2.resize(img, (self.scale, self.scale), interpolation=cv2.INTER_NEAREST)
             self._mic = self._mics_data[self._ids[self._well_id]]
             if hasattr(self, '_im'):
                 self._im.set_data(self._mic)
@@ -149,4 +186,3 @@ class CardioMicFitterMultipleWell(CardioMicFitter):
         
         if not self.check_close():
             self.plot_well()
-            self.draw_plot() 
