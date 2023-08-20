b584b5458b540c9110f24d7676047967e8083182
Bálint Béres <balintberes96@gmail.com>
2023-08-20 22:29:34 +0200

    defs

---
+++ epic_cardio/defs.py
@@ -0,0 +1,6 @@
+WELL_IDS = [['C1', 'C2', 'C3', 'C4'], ['B1', 'B2', 'B3', 'B4'], ['A1', 'A2', 'A3', 'A4']] # Tükrözés korrigálva
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
+
+WELL_NAMES = sorted(flatten(WELL_IDS))
