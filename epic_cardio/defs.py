2480dc3b2afb13ec04acfe3cb861e05325af4a3a
Balint Beres <balintberes96@gmail.com>
2024-02-08 09:47:57 -0800

    added to defs

--- epic_cardio/defs.py
+++ epic_cardio/defs.py
@@ -1,4 +1,5 @@
 WELL_IDS = [['C1', 'C2', 'C3', 'C4'], ['B1', 'B2', 'B3', 'B4'], ['A1', 'A2', 'A3', 'A4']] # Tükrözés korrigálva
+WIDTH = 80
 
 def flatten(t):
     return [item for sublist in t for item in sublist]
